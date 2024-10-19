801f6067a5ed8f193706f351759b2f7ef57165cd
maximatey <maximun154@gmail.com>
2024-10-19 20:58:02 +0700

    add task no 3

---
+++ src/ihpf.m
@@ -0,0 +1,60 @@
+function ihpf = ihpf(image)
+    f = image;
+    imshow(f);
+    [M, N, depth] = size(f);
+    if depth == 3
+        f = rgb2gray(f);
+    end
+    f = im2double(f);
+    
+    % Step 1: Tentukan parameter padding
+    P = 2 * M;
+    Q = 2 * N;
+    
+    % Step 2: Bentuk citra padding
+    fp = zeros(P, Q); % Inisialisasi citra padding
+    fp(1:M, 1:N) = f; % Menyalin citra asli ke bagian atas kiri dari citra padded
+    imshow(fp); title('padded image');
+    
+    % Step 3: Lakukan transformasi Fourier pada fp(x, y)
+    F = fftshift(fft2(fp)); % Transformasi Fourier
+    S2 = log(1 + abs(F)); % Spektrum Fourier
+    figure, imshow(S2, []); title('Fourier spectrum');
+    
+    % Step 4: Bangkitkan fungsi penapis H untuk Ideal Highpass Filter
+    D0 = 50; % cut-off frequency
+
+    % Set up range of variables
+    u = 0:(P-1);
+    v = 0:(Q-1);
+    
+    % Compute indices for use in meshgrid
+    idx = find(u > P/2);
+    u(idx) = u(idx) - P;
+    idy = find(v > Q/2);
+    v(idy) = v(idy) - Q;
+    
+    % Compute meshgrid arrays
+    [V, U] = meshgrid(v, u);
+    D = sqrt(U.^2 + V.^2);
+    
+    % Ideal Highpass Filter
+    H = double(D > D0); % Membuat filter ideal (1 untuk frekuensi lebih dari D0, 0 untuk lainnya)
+    H = fftshift(H); % Pindahkan pusat ke frekuensi
+    
+    figure; imshow(H); title('IHPF Mask');
+    figure, mesh(H);
+    
+    % Step 5: Kalikan F dengan H
+    G = H .* F;
+    G1 = ifftshift(G);
+    
+    % Step 6: Ambil bagian real dari inverse FFT of G
+    G2 = real(ifft2(G1)); % Aplikasi inverse DFT
+    
+    % Step 7: Potong bagian kiri atas sehingga menjadi berukuran citra semula
+    ihpf = G2(1:M, 1:N); % Resize the image untuk menghapus padding
+    
+    % Tampilkan citra output
+    figure; imshow(ihpf); title('Output image after inverse 2D DFT');
+end
