2eb89a0b0837a34889591c8862ee3f815b5273be
maximatey <maximun154@gmail.com>
2024-10-26 02:46:13 +0700

    finish task 6

--- src/periodic_robot.m
+++ src/periodic_robot.m
@@ -1,5 +1,5 @@
-function filtered_image = periodic_car()
-    image = imread('car_periodic.png');
+function filtered_image = periodic_robot()
+    image = imread('robot_periodic.jpg');
     [M,N,~] = size(image);
 
     figure, imshow(image); title('Original Image');
@@ -10,20 +10,13 @@ function filtered_image = periodic_car()
     S2 = log(1+abs(F));
 
     figure, imshow(S2,[]); title ('Fourier Spectrum');
-
-    colsToMod = [39,63,87,115,137,138,139,163,238,239,240];
-
+    colsToMod = [63, 64, 69, 70, 122, 123, 134, 135, 136];
+    
     for i = 1:length(colsToMod)
         F(:, colsToMod(i)) = 0;
         F(:, N - colsToMod(i) + 2) = 0;
     end
 
-    rowsToMod = [24,25,26,92,93,94,156,157,158];
-
-    for i = 1:length(rowsToMod)
-        F(rowsToMod(i),:) = 0;
-        F(M-rowsToMod(i) + 2, :) = 0;
-    end
     S2 = log(1+abs(F));
 
     figure, imshow(S2,[]); title ('Fourier Spectrum After');
