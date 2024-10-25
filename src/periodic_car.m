8df5ee1cb1b8fd90554b825468332ffb61fafad4
maximatey <maximun154@gmail.com>
2024-10-26 02:28:27 +0700

    finish car periodic noise

--- src/periodic_car.m
+++ src/periodic_car.m
@@ -10,6 +10,20 @@ function filtered_image = periodic_car()
     S2 = log(1+abs(F));
 
     figure, imshow(S2,[]); title ('Fourier Spectrum');
+
+    colsToMod = [39,63,87,115,137,138,139,163,238,239,240];
+
+    for i = 1:length(colsToMod)
+        F(:, colsToMod(i)) = 0;
+        F(:, N - colsToMod(i) + 2) = 0;
+    end
+
+    rowsToMod = [24,25,26,92,93,94,156,157,158];
+
+    for i = 1:length(rowsToMod)
+        F(rowsToMod(i),:) = 0;
+        F(M-rowsToMod(i) + 2, :) = 0;
+    end
     S2 = log(1+abs(F));
 
     figure, imshow(S2,[]); title ('Fourier Spectrum After');
