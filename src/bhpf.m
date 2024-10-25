9a7966a1c9d6abe319d158c1a3bb3b5092706dc7
SulthanDA28 <dzakysultan30@gmail.com>
2024-10-25 19:47:08 +0700

    fix: high pass filter

--- src/bhpf.m
+++ src/bhpf.m
@@ -1,10 +1,6 @@
-function bhpf = bhpf(image)
+function bhpf = bhpf(image, DInput)
     f = image;
-    imshow(f);
     [M, N, depth] = size(f);
-    if depth == 3
-        f = rgb2gray(f);
-    end
     f = im2double(f);
     
     % Step 1: Tentukan parameter padding
@@ -12,51 +8,46 @@ function bhpf = bhpf(image)
     Q = 2 * N;
     
     % Step 2: Bentuk citra padding
-    fp = zeros(P, Q); % Inisialisasi citra padding
-    fp(1:M, 1:N) = f; % Menyalin citra asli ke bagian atas kiri dari citra padded
-    imshow(fp); title('padded image');
-    
-    % Step 3: Lakukan transformasi Fourier pada fp(x, y)
-    F = fftshift(fft2(fp)); % Transformasi Fourier
-    S2 = log(1 + abs(F)); % Spektrum Fourier
-    figure, imshow(S2, []); title('Fourier spectrum');
-    
-    % Step 4: Bangkitkan fungsi penapis H untuk Butterworth Highpass Filter
-    D0 = 50; % cut-off frequency
-    n = 2;   % order of the filter
-    
-    % Set up range of variables
-    u = 0:(P-1);
-    v = 0:(Q-1);
-    
-    % Compute indices for use in meshgrid
-    idx = find(u > P/2);
-    u(idx) = u(idx) - P;
-    idy = find(v > Q/2);
-    v(idy) = v(idy) - Q;
-    
-    % Compute meshgrid arrays
-    [V, U] = meshgrid(v, u);
-    D = sqrt(U.^2 + V.^2);
-    
-    % Butterworth Highpass Filter
-    H = 1 ./ (1 + (D0 ./ D).^(2*n)); % Fungsi Butterworth
-    H(D == 0) = 0; % Menangani kasus D = 0
-    H = fftshift(H); % Pindahkan pusat ke frekuensi
-    
-    figure; imshow(H); title('BHPF Butterworth Mask');
-    figure, mesh(H);
-    
-    % Step 5: Kalikan F dengan H
-    G = H .* F;
-    G1 = ifftshift(G);
-    
-    % Step 6: Ambil bagian real dari inverse FFT of G
-    G2 = real(ifft2(G1)); % Aplikasi inverse DFT
+    fp = zeros(P, Q,depth); % Inisialisasi citra padding
+    fp(1:M, 1:N,:) = f; % Menyalin citra asli ke bagian atas kiri dari citra padded
+    matrixresult = fp;
+    for i = 1: depth
+        % Step 3: Lakukan transformasi Fourier pada fp(x, y)
+        F = fftshift(fft2(fp(:,:,i))); % Transformasi Fourier
+        
+        % Step 4: Bangkitkan fungsi penapis H untuk Butterworth Highpass Filter
+        D0 = DInput; % cut-off frequency
+        n = 2;   % order of the filter
+        
+        % Set up range of variables
+        u = 0:(P-1);
+        v = 0:(Q-1);
+        
+        % Compute indices for use in meshgrid
+        idx = find(u > P/2);
+        u(idx) = u(idx) - P;
+        idy = find(v > Q/2);
+        v(idy) = v(idy) - Q;
+        
+        % Compute meshgrid arrays
+        [V, U] = meshgrid(v, u);
+        D = sqrt(U.^2 + V.^2);
+        
+        % Butterworth Highpass Filter
+        H = 1 ./ (1 + (D0 ./ D).^(2*n)); % Fungsi Butterworth
+        H(D == 0) = 0; % Menangani kasus D = 0
+        H = fftshift(H); % Pindahkan pusat ke frekuensi
+        
+        
+        % Step 5: Kalikan F dengan H
+        G = H .* F;
+        G1 = ifftshift(G);
+        
+        % Step 6: Ambil bagian real dari inverse FFT of G
+        matrixresult(:,:,i) = real(ifft2(G1)); % Aplikasi inverse DFT
+    end
     
     % Step 7: Potong bagian kiri atas sehingga menjadi berukuran citra semula
-    bhpf = G2(1:M, 1:N); % Resize the image untuk menghapus padding
+    bhpf = matrixresult(1:M, 1:N,:); % Resize the image untuk menghapus padding
     
-    % Tampilkan citra output
-    figure; imshow(bhpf); title('Output image after inverse 2D DFT');
 end
--- src/ihpf.m
+++ src/ihpf.m
@@ -1,10 +1,6 @@
 function ihpf = ihpf(image,Dinput)
     f = image;
-    imshow(f);
     [M, N, depth] = size(f);
-    if depth == 3
-        f = rgb2gray(f);
-    end
     f = im2double(f);
     
     % Step 1: Tentukan parameter padding
@@ -12,49 +8,43 @@ function ihpf = ihpf(image,Dinput)
     Q = 2 * N;
     
     % Step 2: Bentuk citra padding
-    fp = zeros(P, Q); % Inisialisasi citra padding
-    fp(1:M, 1:N) = f; % Menyalin citra asli ke bagian atas kiri dari citra padded
-    imshow(fp); title('padded image');
-    
-    % Step 3: Lakukan transformasi Fourier pada fp(x, y)
-    F = fftshift(fft2(fp)); % Transformasi Fourier
-    S2 = log(1 + abs(F)); % Spektrum Fourier
-    figure, imshow(S2, []); title('Fourier spectrum');
-    
-    % Step 4: Bangkitkan fungsi penapis H untuk Ideal Highpass Filter
-    D0 = Dinput; % cut-off frequency
+    fp = zeros(P, Q,depth); % Inisialisasi citra padding
+    fp(1:M, 1:N,:) = f; % Menyalin citra asli ke bagian atas kiri dari citra padded
+    matrixresult = fp;
 
-    % Set up range of variables
-    u = 0:(P-1);
-    v = 0:(Q-1);
+    for i=1:depth
+        % Step 3: Lakukan transformasi Fourier pada fp(x, y)
+        F = fftshift(fft2(fp(:,:,i))); % Transformasi Fourier
+        
+        % Step 4: Bangkitkan fungsi penapis H untuk Ideal Highpass Filter
+        D0 = Dinput; % cut-off frequency
     
-    % Compute indices for use in meshgrid
-    idx = find(u > P/2);
-    u(idx) = u(idx) - P;
-    idy = find(v > Q/2);
-    v(idy) = v(idy) - Q;
-    
-    % Compute meshgrid arrays
-    [V, U] = meshgrid(v, u);
-    D = sqrt(U.^2 + V.^2);
-    
-    % Ideal Highpass Filter
-    H = double(D > D0); % Membuat filter ideal (1 untuk frekuensi lebih dari D0, 0 untuk lainnya)
-    H = fftshift(H); % Pindahkan pusat ke frekuensi
-    
-    figure; imshow(H); title('IHPF Mask');
-    figure, mesh(H);
-    
-    % Step 5: Kalikan F dengan H
-    G = H .* F;
-    G1 = ifftshift(G);
-    
-    % Step 6: Ambil bagian real dari inverse FFT of G
-    G2 = real(ifft2(G1)); % Aplikasi inverse DFT
+        % Set up range of variables
+        u = 0:(P-1);
+        v = 0:(Q-1);
+        
+        % Compute indices for use in meshgrid
+        idx = find(u > P/2);
+        u(idx) = u(idx) - P;
+        idy = find(v > Q/2);
+        v(idy) = v(idy) - Q;
+        
+        % Compute meshgrid arrays
+        [V, U] = meshgrid(v, u);
+        D = sqrt(U.^2 + V.^2);
+        
+        % Ideal Highpass Filter
+        H = double(D > D0); % Membuat filter ideal (1 untuk frekuensi lebih dari D0, 0 untuk lainnya)
+        H = fftshift(H); % Pindahkan pusat ke frekuensi
+        
+        % Step 5: Kalikan F dengan H
+        G = H .* F;
+        G1 = ifftshift(G);
+        
+        % Step 6: Ambil bagian real dari inverse FFT of G
+        matrixresult(:,:,i) = real(ifft2(G1)); % Aplikasi inverse DFT
+    end
     
     % Step 7: Potong bagian kiri atas sehingga menjadi berukuran citra semula
-    ihpf = G2(1:M, 1:N); % Resize the image untuk menghapus padding
-    
-    % Tampilkan citra output
-    figure; imshow(ihpf); title('Output image after inverse 2D DFT');
+    ihpf = matrixresult(1:M, 1:N,:); % Resize the image untuk menghapus padding
 end
