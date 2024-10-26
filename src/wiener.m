aeab898c5ba6de23432ae879d6dbcc76b49db556
SulthanDA28 <dzakysultan30@gmail.com>
2024-10-26 19:44:35 +0700

    fix: wiener filter

--- src/wiener.m
+++ src/wiener.m
@@ -4,16 +4,16 @@ function wiener = wiener (blured,psf,K)
     image = im2double(blured);
     H = fft2(psf,M,N);
 
+    abskuadrath = conj(H) .* H;
+    wienerfilter = (1./H) .* (abskuadrath./(abskuadrath + K));
+
     hasil = zeros(size(blured));
 
     for i = 1: depth
         channel = image(:,:,i);
         G = fft2(channel);
-
-        abskuadrath = conj(H) .* H;
-        wienerchannel = (1./H) .* (abskuadrath./(abskuadrath + K));
         
-        hasilchanel = G .* wienerchannel;
+        hasilchanel = G .* wienerfilter;
 
         restore_chanel = ifft2(hasilchanel);
 
